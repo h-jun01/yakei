1e4dcb5741c2596c7a07bc1d8b97d680acb57d32
keitakageyama <kogeyama3@gmail.com>
2020-06-16 12:18:29 +0900

    googlemapの作成

--- App.js
+++ App.js
@@ -1,44 +1,20 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
-
-const App = () => {
-  return (
-    <View style={styles.lap}>
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+export default class App extends React.Component {
+  render() {
+    return (
       <MapView
-        style={{ flex: 3, zIndex: 1 }}
+        style={{ flex: 1 }}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
         initialRegion={{
-          latitude: 35.681236,
-          longitude: 139.767125,
-          latitudeDelta: 0.02, //小さくなるほどズーム
-          longitudeDelta: 0.02,
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
-      >
-        <MapView.Marker
-          coordinate={{
-            latitude: 35.681236,
-            longitude: 139.767125,
-          }}
-          title={"東京駅"}
-          description={"駅"}
-          // onPress={() => alert("click")}
-        />
-      </MapView>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  lap: {
-    backgroundColor: "rgba(0,0,0,0.6)",
-    flex: 99,
-    zIndex: 2,
-    position: "absolute",
-    left: 0,
-    right: 0,
-    bottom: 0,
-    top: 0,
-  },
-});
-
-export default App;
+      />
+    );
+  }
+}
