27e00825a772abc3dde492231b3405d4bfc9603b
橋本准 <hashimotojun@jun.local>
2020-06-23 01:14:30 +0900

    bottomNavの骨格作成

--- App.js
+++ App.js
@@ -24,20 +24,48 @@ const App = () => {
           // onPress={() => alert("click")}
         />
       </MapView>
+      <View style={styles.bottomNav}>
+        <Text>テスト</Text>
+        <Text>テスト</Text>
+        <Text style={styles.bottomNavCenter}>テスト</Text>
+        <Text>テスト</Text>
+        <Text>テスト</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   lap: {
-    backgroundColor: "rgba(0,0,0,0.6)",
-    flex: 99,
-    zIndex: 2,
+    // backgroundColor: "rgba(0,0,0,0.6)",
+    // flex: 99,
+    // zIndex: 2,
     position: "absolute",
     left: 0,
     right: 0,
     bottom: 0,
     top: 0,
+  },
+  bottomNav: {
+    position: "absolute",
+    bottom: 0,
+    zIndex: 2,
+    height: 75,
+    width: "100%",
+    backgroundColor: "#fff",
+    paddingBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  bottomNavCenter: {
+    height: 120,
+    lineHeight: 60,
+    borderWidth: 30,
+    borderRadius: 45,
+    overflow: "hidden",
+    borderColor: "#fff",
+    backgroundColor: "#fff",
   },
 });
 
