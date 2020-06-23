116ac07c5ad84efd6519290c24a097ed437bf949
橋本准 <hashimotojun@jun.local>
2020-06-23 12:56:43 +0900

    ナビだけにした

--- App.js
+++ App.js
@@ -1,29 +1,18 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  FlatList,
+  Dimensions,
+} from "react-native";
+// import MapView from "react-native-maps";
 
 const App = () => {
   return (
-    <View style={styles.lap}>
-      <MapView
-        style={{ flex: 3, zIndex: 1 }}
-        initialRegion={{
-          latitude: 35.681236,
-          longitude: 139.767125,
-          latitudeDelta: 0.02, //小さくなるほどズーム
-          longitudeDelta: 0.02,
-        }}
-      >
-        <MapView.Marker
-          coordinate={{
-            latitude: 35.681236,
-            longitude: 139.767125,
-          }}
-          title={"東京駅"}
-          description={"駅"}
-          // onPress={() => alert("click")}
-        />
-      </MapView>
+    <SafeAreaView style={styles.lap}>
       <View style={styles.bottomNav}>
         <Text>テスト</Text>
         <Text>テスト</Text>
@@ -31,29 +20,22 @@ const App = () => {
         <Text>テスト</Text>
         <Text>テスト</Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   lap: {
-    // backgroundColor: "rgba(0,0,0,0.6)",
-    // flex: 99,
-    // zIndex: 2,
-    position: "absolute",
-    left: 0,
-    right: 0,
-    bottom: 0,
-    top: 0,
+    backgroundColor: "#000",
+    flex: 1,
   },
   bottomNav: {
     position: "absolute",
     bottom: 0,
     zIndex: 2,
-    height: 75,
+    height: 80,
     width: "100%",
     backgroundColor: "#fff",
-    paddingBottom: 20,
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
