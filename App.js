e24f8ae3684bbe792ce18dd1edf65e66dcb6ec92
橋本准 <hashimotojun@jun.local>
2020-06-23 12:01:18 +0900

    詳細の骨格

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  FlatList,
+  Dimensions,
+} from "react-native";
+// import MapView from "react-native-maps";
+
+const ITEM_WIDTH = Dimensions.get("window").width;
 
 const App = () => {
+  const state = {
+    list: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
+  };
   return (
-    <View style={styles.lap}>
-      <MapView
-        style={{ flex: 3, zIndex: 1 }}
-        initialRegion={{
-          latitude: 35.681236,
-          longitude: 139.767125,
-          latitudeDelta: 0.02, //小さくなるほどズーム
-          longitudeDelta: 0.02,
-        }}
-      >
-        <MapView.Marker
-          coordinate={{
-            latitude: 35.681236,
-            longitude: 139.767125,
-          }}
-          title={"東京駅"}
-          description={"駅"}
-          // onPress={() => alert("click")}
-        />
-      </MapView>
+    <SafeAreaView style={styles.lap}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>清水寺</Text>
+      </View>
+      <FlatList
+        style={styles.main}
+        data={state.list}
+        keyExtractor={(item, index) => index.toString()}
+        numColumns={3}
+        renderItem={({ item }) => (
+          <View>
+            <Image
+              source={{ uri: "https://source.unsplash.com/random" }}
+              style={styles.imageStyle}
+            />
+          </View>
+        )}
+      />
+      {/* <Image style={styles.tinyLogo} source={require("./assets/test.png")} /> */}
       <View style={styles.bottomNav}>
         <Text>テスト</Text>
         <Text>テスト</Text>
@@ -31,29 +43,43 @@ const App = () => {
         <Text>テスト</Text>
         <Text>テスト</Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   lap: {
-    // backgroundColor: "rgba(0,0,0,0.6)",
-    // flex: 99,
-    // zIndex: 2,
-    position: "absolute",
-    left: 0,
-    right: 0,
-    bottom: 0,
-    top: 0,
+    backgroundColor: "#000",
+    flex: 1,
+  },
+  header: {
+    height: 70,
+    borderWidth: 1,
+    borderBottomColor: "#FF8016",
+  },
+  headerTitle: {
+    color: "#fff",
+    textAlign: "center",
+    lineHeight: 70,
+    fontSize: 17,
+    fontWeight: "bold",
+  },
+  main: {
+    marginBottom: 50,
+  },
+  imageStyle: {
+    width: ITEM_WIDTH / 3,
+    height: ITEM_WIDTH / 3,
+    margin: 1,
+    resizeMode: "cover",
   },
   bottomNav: {
     position: "absolute",
     bottom: 0,
     zIndex: 2,
-    height: 75,
+    height: 80,
     width: "100%",
     backgroundColor: "#fff",
-    paddingBottom: 20,
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
