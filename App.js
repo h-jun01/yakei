8ce729f8c14ff4dda905ddecba6f1f8c708bd696
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-06-26 16:57:59 +0900

    first commit

--- App.js
+++ App.js
@@ -1,44 +1,28 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import MapView from "react-native-maps";
-
-const App = () => {
-  return (
-    <View style={styles.lap}>
-      <MapView
-        style={{ flex: 3, zIndex: 1 }}
-        initialRegion={{
-          latitude: 35.681236,
-          longitude: 139.767125,
-          latitudeDelta: 0.02, //小さくなるほどズーム
-          longitudeDelta: 0.02,
-        }}
-      >
-        <MapView.Marker
-          coordinate={{
-            latitude: 35.681236,
-            longitude: 139.767125,
-          }}
-          title={"東京駅"}
-          description={"駅"}
-          // onPress={() => alert("click")}
-        />
-      </MapView>
-    </View>
-  );
-};
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  lap: {
-    backgroundColor: "rgba(0,0,0,0.6)",
-    flex: 99,
-    zIndex: 2,
-    position: "absolute",
-    left: 0,
-    right: 0,
-    bottom: 0,
-    top: 0,
-  },
-});
+  center: {
+    alignItems: 'center'
+  }
+})
 
-export default App;
+const Greeting = (props) => {
+  return (
+    <View style={styles.center}>
+      <Text>Hello {props.name}!</Text>
+    </View>
+  );
+}
+
+const LotsOfGreetings = () => {
+  return (
+    <View style={[styles.center, {top: 50}]}>
+      <Greeting name='Rexxar' />
+      <Greeting name='Jaina' />
+      <Greeting name='Valeera' />
+    </View>
+  );
+}
+
+export default LotsOfGreetings;
