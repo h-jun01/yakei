841e83b6365487619cd891762a5f287e70714c1d
橋本准 <hashimotojun@jun.local>
2020-06-30 12:35:29 +0900

    commit

--- App.js
+++ App.js
@@ -17,14 +17,14 @@ import * as firebase from "firebase";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAb00UD1DkTptJejh92XaVLYROk34nICy4",
-  authDomain: "hal-yakei.firebaseapp.com",
-  databaseURL: "https://hal-yakei.firebaseio.com",
-  projectId: "hal-yakei",
-  storageBucket: "hal-yakei.appspot.com",
-  messagingSenderId: "655737634399",
-  appId: "1:655737634399:web:54120719f995fcdfb3e5ea",
-  measurementId: "G-B7TM18STEN",
+  // apiKey: "AIzaSyAb00UD1DkTptJejh92XaVLYROk34nICy4",
+  // authDomain: "hal-yakei.firebaseapp.com",
+  // databaseURL: "https://hal-yakei.firebaseio.com",
+  // projectId: "hal-yakei",
+  // storageBucket: "hal-yakei.appspot.com",
+  // messagingSenderId: "655737634399",
+  // appId: "1:655737634399:web:54120719f995fcdfb3e5ea",
+  // measurementId: "G-B7TM18STEN",
 };
 // Initialize Firebase
 
