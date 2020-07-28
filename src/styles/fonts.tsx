6dedceb64a765c0674ca050afe5e2d3a8b1825d2
keitakageyama <kogeyama3@gmail.com>
2020-07-28 10:32:32 +0900

    color.tsxとfonts.tsxの追加

---
+++ src/styles/fonts.tsx
@@ -0,0 +1,16 @@
+export const fontSize = {
+  fontSizeXxsmall: "8",
+  fontSizeXsmall: "10",
+  fontSizeSmall: "12",
+  fontSizeNormalS: "13",
+  fontSizeNormal: "14",
+  fontSizeLarge: "16",
+  fontSizeXlarge: "18",
+  fontSizeXxlarge: "20",
+  lineHeight: "1.5",
+};
+export const fontFamily = {
+  gothic: "YuGothic",
+  roboto:
+    'Roboto, Helvetica, Arial, "Hiragino Sans", "Hiragino Kaku Gothic Pro", "ヒラギノ角ゴ Pro W3", メイリオ, Meiryo, sans-serif',
+};
