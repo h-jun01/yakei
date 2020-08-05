55d13c6b2aa9eb25a4d41d3df1b4e4427646b22f
h-jun01 <jyun.0110@icloud.com>
2020-08-05 19:59:41 +0900

    [fix]validationCheck

--- src/containers/organisms/SignIn.tsx
+++ src/containers/organisms/SignIn.tsx
@@ -5,6 +5,7 @@ import SignIn from "../../componets/organisms/SignIn";
 import { accountFireStore } from "../../firebase/accountFireStore";
 import { useInput } from "../../utilities/hooks/input";
 import { StackParamList } from "../../index";
+import { callingAlert } from "../../utilities/alert";
 
 type UseInput = {
   value: string;
@@ -46,9 +47,31 @@ const ContainerAuth: FC<Props> = ({ navigation }) => {
 
   //新規登録処理
   const signInUser = async (email: string, password: string) => {
+    const REGEX_EMAIL = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const REGEX_PASSWORD = /^(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}$/i;
     try {
-      accountFireStore.loginUser({ email, password });
+      if (!email) {
+        callingAlert("メールアドレスを入力してください");
+        return;
+      } else if (!email.match(REGEX_EMAIL)) {
+        callingAlert("メールドレスの形式が不正です");
+        return;
+      } else if (!password.match(REGEX_PASSWORD)) {
+        callingAlert(
+          "パスワードは半角英数字を含めた6文字以上で入力してください"
+        );
+        return;
+      }
+      setIsLoading(true);
+
+      await accountFireStore.loginUser({ email, password }).catch((error) => {
+        console.log(error.toString());
+        setIsLoading(false);
+        callingAlert("メールアドレスまたはパスワードが違います。");
+        return;
+      });
     } catch (error) {
+      setIsLoading(false);
       console.log(error.toString());
     }
   };
