ab925aed6d136898d9f22190120b5c2c29a7ee41
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-07 19:23:53 +0900

    Add: テストとして、今までのコードをコメントアウトしてTab Navigationのサンプルコードを追加した
    Update:
    Tab Navigationを使う際にエラーが表示されたため、expoのバージョンを37.0.0から38.0.0にアップグレードした
    
    Add:
    テストとして、今までのコードをコメントアウトしてTab Navigationのサンプルコードを追加した。
    また、Tab Navigationを使う際にエラーが表示されたため、react-native-gesture-handler、react-native-reanimated、react-native-screensの3つのパッケージをインストールした。

--- src/components/organisms/BottomNav.tsx
+++ src/components/organisms/BottomNav.tsx
@@ -1,5 +1,7 @@
 import React, { FC } from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import FooterBackgroundSvg from "../atoms/svg/FooterBackgroundSvg";
 import MapButtonSvg from "../atoms/svg/MapButtonSvg";
@@ -8,18 +10,42 @@ import PlusButtonSvg from "../atoms/svg/PlusButtonSvg";
 import NotificationButtonSvg from "../atoms/svg/NotificationButtonSvg";
 import RoundedUserImage from "../atoms/RoundedUserImage";
 
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home!</Text>
+    </View>
+  );
+};
+
+const SettingsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+};
+
 const BottomNavComponent: FC = () => {
   return (
-    <View style={styles.container}>
-      <FooterBackgroundSvg style={styles.fotterItem} />
-      <View style={styles.fotterItem}>
-        <MapButtonSvg />
-        <FolderButtonSvg />
-        <PlusButtonSvg />
-        <NotificationButtonSvg />
-        <RoundedUserImage />
-      </View>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <FooterBackgroundSvg style={styles.fotterItem} />
+    //   <View style={styles.fotterItem}>
+    //     <MapButtonSvg />
+    //     <FolderButtonSvg />
+    //     <PlusButtonSvg />
+    //     <NotificationButtonSvg />
+    //     <RoundedUserImage />
+    //   </View>
+    // </View>
   );
 };
 
