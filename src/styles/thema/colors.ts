1704b7fb61aa327fcac0833861578fb89640c8f6
kii hira <hira12.tai11@gmail.com>
2020-08-21 18:18:35 +0900

    fix:競合の解決

--- src/styles/thema/colors.ts
+++ src/styles/thema/colors.ts
@@ -2,7 +2,6 @@ import { Overlay } from "react-native-maps"
 
 // Service Colors
 export const baseColor = {
-<<<<<<< HEAD
   primary: '#39a869',
   primaryDarker: '#00783e',
   primaryLite: '#00b747',
@@ -11,17 +10,6 @@ export const baseColor = {
   text: '#fff',
   main: '#4CAF7C',
 }
-=======
-  primary: "#39a869",
-  primaryDarker: "#00783e",
-  primaryLite: "#00b747",
-  accent: "#21A6B8",
-  base: "#272E45",
-  text: "#fff",
-  main: "#4CAF7C",
-};
->>>>>>> 0172dbd4be2fb596a4ef54e6c26db37b9cf0af35
-
 // Utility Colors
 export const utilityColor = {
   border: "#fff",
@@ -30,30 +18,9 @@ export const utilityColor = {
   attention: "#e8ac51",
   authNav: "rgba(62,64,75,0.8)",
   inputBack: "rgba(62,64,75,0.9)",
-<<<<<<< HEAD
   editBox: 'rgb(51, 124, 234)',
   placeholderText: 'rgba(204, 204, 204,0.7)',
   inputLabel: '#A7ABB1',
   overlay: 'rgba(65,65,65,0.6)',
   strongOverlay: 'rgba(0,0,0,0.4)',
 }
-=======
-  editBox: "rgb(51, 124, 234)",
-  placeholderText: "rgba(204, 204, 204,0.7)",
-  // attentionSub: '#fe446d',
-  // attentionBg: '#faeedc',
-  // warning: '#de6641',
-  // warningBg: '#f8e0d9',
-  // warningButtonBg: '#bdbdbd',
-  // warningButton: '#606060',
-};
-
-// text Colors
-export const fontColor = {
-  // sub: ' #667587',
-  // main: '#313541',
-  // error: '#c62828',
-  // success: '#33691e',
-  // buttonWhite: '#fff',
-};
->>>>>>> 0172dbd4be2fb596a4ef54e6c26db37b9cf0af35
