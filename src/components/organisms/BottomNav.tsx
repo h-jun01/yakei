c23ad39bc3125ae9e8db667ef93ceedee43249f9
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-08-24 08:08:07 +0900

    Remove: SafeAreaViewがなくても問題なく動作したためその関係の処理を削除

--- src/components/organisms/BottomNav.tsx
+++ src/components/organisms/BottomNav.tsx
@@ -1,11 +1,5 @@
-import React, { FC, useState } from "react";
-import {
-  View,
-  SafeAreaView,
-  TouchableOpacity,
-  Dimensions,
-  StyleSheet,
-} from "react-native";
+import React, { FC } from "react";
+import { View, TouchableOpacity, Dimensions, StyleSheet } from "react-native";
 import type { BottomTabBarProps as Props } from "@react-navigation/bottom-tabs/lib/typescript/src/types";
 import { baseColor } from "../../styles/thema/colors";
 
@@ -13,75 +7,59 @@ import FooterBackgroundSvg from "../atoms/svg/FooterBackgroundSvg";
 import BottomNavItem from "../../containers/molecules/BottomNavItem";
 
 const BottomNav: FC<Props> = ({ state, descriptors, navigation }) => {
-  const [safeAreaInsetHeighet, setSafeAreaInsetHeihgt] = useState(0);
-  const onLayout = (e) => {
-    setSafeAreaInsetHeihgt(e.nativeEvent.layout.height);
-  };
-
-  const footerSvg = StyleSheet.create({
-    bottom: { bottom: -safeAreaInsetHeighet },
-  });
-  const footerSvgStyle = { ...styles.footerSvg, ...footerSvg.bottom };
-
   return (
-    <>
-      <View style={styles.container}>
-        <View style={styles.footerSvgWrap}>
-          <FooterBackgroundSvg
-            style={footerSvgStyle}
-            backColor={baseColor.darkNavy}
-          />
-        </View>
-        <View style={styles.footerWrap}>
-          {state.routes.map((route, index) => {
-            const { options } = descriptors[route.key];
-            const label = route.name;
-            const isFocused = state.index === index;
-
-            const onPress = () => {
-              const event = navigation.emit({
-                type: "tabPress",
-                target: route.key,
-                canPreventDefault: true,
-              });
-              if (!isFocused && !event.defaultPrevented) {
-                navigation.navigate(route.name);
-              }
-            };
-
-            const onLongPress = () => {
-              navigation.emit({
-                type: "tabLongPress",
-                target: route.key,
-              });
-            };
-
-            return (
-              <TouchableOpacity
-                key={index}
-                accessibilityRole="button"
-                accessibilityState={isFocused ? { selected: true } : {}}
-                accessibilityLabel={options.tabBarAccessibilityLabel}
-                onPress={index !== 2 ? onPress : () => {}}
-                onLongPress={onLongPress}
-                activeOpacity={1}
-                style={[
-                  styles.footerItem,
-                  index === 2 ? styles.plusButton : {},
-                ]}
-              >
-                <BottomNavItem
-                  index={index}
-                  isFocused={isFocused}
-                  label={label}
-                />
-              </TouchableOpacity>
-            );
-          })}
-        </View>
+    <View style={styles.container}>
+      <View style={styles.footerSvgWrap}>
+        <FooterBackgroundSvg
+          style={styles.footerSvg}
+          backColor={baseColor.darkNavy}
+        />
       </View>
-      <SafeAreaView style={{ display: "none" }} onLayout={onLayout} />
-    </>
+      <View style={styles.footerWrap}>
+        {state.routes.map((route, index) => {
+          const { options } = descriptors[route.key];
+          const label = route.name;
+          const isFocused = state.index === index;
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name);
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: "tabLongPress",
+              target: route.key,
+            });
+          };
+
+          return (
+            <TouchableOpacity
+              key={index}
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              onPress={index !== 2 ? onPress : () => {}}
+              onLongPress={onLongPress}
+              activeOpacity={1}
+              style={[styles.footerItem, index === 2 ? styles.plusButton : {}]}
+            >
+              <BottomNavItem
+                index={index}
+                isFocused={isFocused}
+                label={label}
+              />
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </View>
   );
 };
 
