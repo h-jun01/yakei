25a4ceff72dac51fb207bb90eeaaaf7022d25dc2
h-jun01 <jyun.0110@icloud.com>
2020-08-28 10:18:29 +0900

    Fix:postedの型を作成

--- src/actions/postedData.ts
+++ src/actions/postedData.ts
@@ -1,14 +1,8 @@
 import { ActionTypes } from "./index";
 
-type CommentDataList = {
-  uid: string;
-  message: string;
-  create_time: string;
-};
-
 //コメント一覧をセット
 export const setCommentDataList = (
-  commentDataList: CommentDataList[] | firebase.firestore.DocumentData[]
+  commentDataList: firebase.firestore.DocumentData[]
 ) =>
   ({
     type: ActionTypes.COMMENT_DATA_LIST,
