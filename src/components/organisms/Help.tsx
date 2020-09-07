462269f1db23bea1ad910df4746ec11fa5d1a4ee
h-jun01 <jyun.0110@icloud.com>
2020-09-08 02:24:51 +0900

    Add:ヘルプに必要に項目を追加

--- src/components/organisms/Help.tsx
+++ src/components/organisms/Help.tsx
@@ -7,7 +7,29 @@ const Help: FC = () => {
     <View style={styles.container}>
       <HelpHeading heading="注意事項" />
       <Text>・他人の写真をYAKEIに投稿してはいけません。</Text>
+      <Text>
+        ・掲載されている他人の作品をブログやサイト等に無断で転載してはいけません。
+      </Text>
+      <Text>・無差別な商業用の宣伝行為をしてはいけません</Text>
+      <Text>
+        ・ 他の利用者に対して誹謗・中傷・プライバシーの侵害をしてはいけません
+      </Text>
       <HelpHeading heading="よくある質問" />
+      <Text style={styles.questionText}>
+        登録しているメールアドレスを変更したい
+      </Text>
+      <Text style={styles.questionText}>
+        登録しているパスワードを変更したい
+      </Text>
+      <Text style={styles.questionText}>
+        パスワード変更時にメールが届きません
+      </Text>
+      <Text style={styles.questionText}>YAKEIを退会したい</Text>
+      <Text style={styles.questionText}>
+        投稿写真を保存するにはどのようにすればいいですか？
+      </Text>
+      <Text style={styles.questionText}>スポットとはなんですか？</Text>
+      <Text style={styles.questionText}>ピックアップとはなんですか？</Text>
     </View>
   );
 };
@@ -15,6 +37,9 @@ const Help: FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  questionText: {
+    color: "#1D89C6",
   },
 });
 
