022b986d719e0a602115706936cfef9fb4be5e26
h-jun01 <jyun.0110@icloud.com>
2020-09-08 13:01:33 +0900

    Add:質問のレイアウトを作成

--- src/components/atoms/FaqHeading.tsx
+++ src/components/atoms/FaqHeading.tsx
@@ -1,22 +1,22 @@
 import React, { FC } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { Text, StyleSheet } from "react-native";
+import { Size } from "../../styles/thema/fonts";
 
 type Props = {
   heading: string;
 };
 
 const FaqHeading: FC<Props> = ({ heading }) => {
-  return (
-    <View>
-      <Text style={styles.textColor}>{heading}</Text>
-    </View>
-  );
+  return <Text style={styles.textColor}>{heading}</Text>;
 };
 
 const styles = StyleSheet.create({
   textColor: {
+    fontSize: Size.Large,
     fontWeight: "bold",
     color: "#303030",
+    marginTop: 20,
+    marginBottom: 20,
   },
 });
 
