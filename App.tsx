75467b073355b18530d9c8ba57995cbf18cd286e
h-jun01 <jyun.0110@icloud.com>
2020-09-08 23:56:10 +0900

    Fix:androidのstatusbarの色を変更

--- App.tsx
+++ App.tsx
@@ -16,7 +16,7 @@ if (!global.atob) {
 if (Platform.OS === "ios") {
   StatusBar.setBarStyle("light-content", true);
 } else if (Platform.OS === "android") {
-  StatusBar.setBackgroundColor("#fff", true);
+  StatusBar.setBackgroundColor("#181f32", true);
 }
 
 const store = createStore(rootReducer);
