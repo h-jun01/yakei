2b3a20f6e648af27523c0e70f970451c06d3f61d
ryoga <47961006+big-Miyabi@users.noreply.github.com>
2020-09-08 17:43:33 +0900

    Add: bottomNavのマイページボタンにユーザーアイコンを表示する処理の追加

--- src/components/atoms/RoundedUserImage.tsx
+++ src/components/atoms/RoundedUserImage.tsx
@@ -4,17 +4,14 @@ import { StyleSheet, Image } from "react-native";
 type Props = {
   aspect: number;
   borderWidth: number;
+  url: string;
   color?: string;
-};
-
-const source = {
-  uri:
-    "https://firebasestorage.googleapis.com/v0/b/hal-yakei.appspot.com/o/users%2F8HG1hZgvW7WiXA1oaaPMyn59ayB2%2Fuser.jpeg?alt=media&token=cb63a15b-d239-4543-9e1c-d45e1932bb98",
 };
 
 const RoundedUserImageComponent: FC<Props> = ({
   aspect,
   borderWidth,
+  url,
   color = "#838692",
 }) => {
   const styles = StyleSheet.create({
@@ -27,7 +24,7 @@ const RoundedUserImageComponent: FC<Props> = ({
 
   return (
     <Image
-      source={source}
+      source={{ uri: url }}
       style={[{ width: aspect, height: aspect }, styles.image]}
     />
   );
