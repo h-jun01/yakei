05d19e72acc51ea7ad805ff9b6d5dc5f6ac00555
h-jun01 <jyun.0110@icloud.com>
2020-09-15 10:44:18 +0900

    Update:よくある質問のスタイルをiPad対応

--- src/components/atoms/FaqHeading.tsx
+++ src/components/atoms/FaqHeading.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from "react";
+import { Platform, PlatformIOSStatic } from "react-native";
 import { Text, StyleSheet } from "react-native";
 import { Size } from "../../styles/thema/fonts";
+import { widthPercentageToDP as wp } from "react-native-responsive-screen";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
 
 type Props = {
   heading: string;
@@ -10,9 +13,11 @@ const FaqHeading: FC<Props> = ({ heading }) => {
   return <Text style={styles.textColor}>{heading}</Text>;
 };
 
+const platformIOS = Platform as PlatformIOSStatic;
+
 const styles = StyleSheet.create({
   textColor: {
-    fontSize: Size.Large,
+    fontSize: platformIOS.isPad ? Size.NormalS : Size.Large,
     fontWeight: "bold",
     color: "#303030",
     marginTop: 20,
